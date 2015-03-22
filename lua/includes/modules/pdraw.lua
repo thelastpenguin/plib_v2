97ccf72181b4b982e3612ba6e86db205df8264d6
aStonedPenguin <zach@superiorservers.co>
2015-03-21 20:39:45 -0800

    Added draw utils

--- lua/includes/modules/pdraw.lua
+++ lua/includes/modules/pdraw.lua
@@ -1,70 +1,48 @@
---[[LICENSE:
-_p_modules\lua\includes\modules\pdraw.luasrc
+local surface 	= surface
+local render 	= render
 
-Copyright 08/24/2014 thelastpenguin
-]]
--- DRAW QUAD
+function draw.Box(x, y, w, h, col)
+	surface.SetDrawColor(col)
+	surface.DrawRect(x, y, w, h)
+end
+
+function draw.Outline(x, y, w, h, col, thickness)
+	for i = 1, (thickness or 1) do
+		surface.SetDrawColor(col)
+		surface.DrawOutlinedRect(x + (i - 1), y + (i - 1), w - ((i - 1) * 2), h - ((i - 1) * 2))
+	end
+end
+
+function draw.OutlinedBox(x, y, w, h, col, bordercol, thickness)
+	surface.SetDrawColor(col)
+	surface.DrawRect(x + 1, y + 1, w - 2, h - 2)
+
+	draw.Outline(x, y, w, h, bordercol, thickness)
+end
+
+local blur = Material('pp/blurscreen')
+function draw.Blur(panel, amount) -- Thanks nutscript
+	local x, y = panel:LocalToScreen(0, 0)
+	local scrW, scrH = ScrW(), ScrH()
+	surface.SetDrawColor(255, 255, 255)
+	surface.SetMaterial(blur)
+	for i = 1, 3 do
+		blur:SetFloat('$blur', (i / 3) * (amount or 6))
+		blur:Recompute()
+		render.UpdateScreenEffectTexture()
+		surface.DrawTexturedRect(x * -1, y * -1, scrW, scrH)
+	end
+end
+
+-- Begin the moonshit
 do
 	local q = {{},{},{},{}};
-	local q1, q2, q3, q4 = q[1], q[2], q[3], q[4]
-	local drawpoly = surface.DrawPoly
 	function surface.DrawQuad( x1, y1, x2, y2, x3, y3, x4, y4 )
-		q1.x, q1.y = x1, y1;
-		q2.x, q2.y = x2, y2;
-		q3.x, q3.y = x3, y3;
-		q4.x, q4.y = x4, y4;
-		drawpoly(q);
-	end
-
-	local quv = {{},{},{},{}}
-	local quv1, quv2, quv3, quv4 = quv[1], quv[2], quv[3], quv[4]
-	local math_min, math_max = math.min, math.max
-	function surface.DrawQuadUV( x1, y1, x2, y2, x3, y3, x4, y4 )
-		local xmin, ymin = math_max, math_max
-		local xmax, ymax = math_min, math_min
-
-		xmin = x1
-		if x2 < xmin then xmin = x2 end
-		if x3 < xmin then xmin = x3 end
-		if x4 < xmin then xmin = x4 end
-
-		ymin = y1
-		if y2 < ymin then ymin = y2 end
-		if y3 < ymin then ymin = y3 end
-		if y4 < ymin then ymin = y4 end
-
-		xmax = x1
-		if x2 > xmax then xmax = x2 end
-		if x3 > xmax then xmax = x3 end
-		if x4 > xmax then xmax = x4 end
-		
-		ymax = y1
-		if y2 > ymax then ymax = y2 end
-		if y3 > ymax then ymax = y3 end
-		if y4 > ymax then ymax = y4 end
-
-		local dy = ymax - ymin
-		local dx = xmax - xmin
-
-		quv1.u, quv1.v = (x1-xmin)/dx, (y1-ymin)/dy
-		quv2.u, quv2.v = (x2-xmin)/dx, (y2-ymin)/dy
-		quv3.u, quv3.v = (x3-xmin)/dx, (y3-ymin)/dy
-		quv4.u, quv4.v = (x4-xmin)/dx, (y4-ymin)/dy
-
-		quv1.x, quv1.y = x1, y1
-		quv2.x, quv2.y = x2, y2
-		quv3.x, quv3.y = x3, y3
-		quv4.x, quv4.y = x4, y4
-
-		drawpoly(quv)
-	end
-
-	local drawline = surface.DrawLine
- 	function surface.DrawOutlinedQuad(x1, y1, x2, y2, x3, y3, x4, y4)
- 		drawline(x1,y1, x2,y2)
- 		drawline(x2,y2, x3,y3)
- 		drawline(x3,y3, x4,y4)
- 		drawline(x4,y4, x1,y1)
+		q[1].x, q[1].y = x1, y1;
+		q[2].x, q[2].y = x2, y2;
+		q[3].x, q[3].y = x3, y3;
+		q[4].x, q[4].y = x4, y4;
+		surface.DrawPoly( q );
 	end
 end
 
@@ -166,8 +144,8 @@ do
     local textW, textH = surface_GetTextSize(font)
     angle.y = ang
     vector.x, vector.y = x, y
-    matrix:SetAngles(angle)
     matrix:SetTranslation(vector)
+    matrix:SetAngles(angle)
     cam_PushModelMatrix(matrix)
     surface_DrawText(text)
     return cam_PopModelMatrix()
